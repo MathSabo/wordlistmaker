7499508dbc81e3fc14e4197137428191c9cac4f4
MathSabo <110696037+MathSabo@users.noreply.github.com>
2022-10-24 03:11:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]
-	then echo "Please run as root."
-	exit 1
+        then echo "Please run as root."
+        exit 1
 fi
 
-
 apt update
-apt install curl -y
+apt install curl git -y
 
-chmod 755 wordlistmaker/wordlistmaker
-cp wordlistmaker/wordlismaker /usr/bin/wordlistmaker
+git clone https://github.com/MathSabo/wordlistmaker
+chmod 755 /wordlistmaker/wordlistmaker
+cp /wordlistmaker/wordlismaker /usr/bin/wordlistmaker
 rm -rf wordlismaker
 
 echo "=================================================="
 echo "====== Thanks for using my tool! - MathSabo ======"
 echo "=================================================="
-
